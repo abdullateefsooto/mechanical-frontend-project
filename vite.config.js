91f351dc9f5567e904d853f985cd7737414b3927
abdullateef <abdullateefopeyemi06@gmail.com>
2026-01-16 00:31:18 +0100

    fix 404 on refresh

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react(),],
-})
-
-
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  base: "/", // use "/" if hosting at root
+});
